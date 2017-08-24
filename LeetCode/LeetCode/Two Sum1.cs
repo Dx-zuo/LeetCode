65311a256210ba541d9533d127db0a0f9259a58d
tooks ab <error-oo@outlook.com>
2017-08-24 14:41:58 +0800

    1. Two Sum

---
+++ LeetCode/LeetCode/Two Sum1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    class Two_Sum1
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            for (int x = 0; x < nums.Length; x++)
+            {
+                for (int y = nums.Length - 1; y >= 0; y--)
+                {
+                    if (nums[x] != nums[y])
+                    {
+                        if ((nums[x] + nums[y]) == target)
+                        {
+                            return new int[] { x, y };
+                        }
+                    }
+                }
+            }
+            return null;
+        }
+    }
+}
